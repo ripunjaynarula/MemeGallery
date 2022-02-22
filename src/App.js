65ea40645324f9658c7c0c5fed01ba79d5b154cf
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2022-02-22 20:02:48 +0530

    final meme portal

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Nav from "./Nav";
+import idl from "./idl.json";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
+import kp from './keypair.json'
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram, Keypair } = web3;
+
+// Create a keypair for the account that will hold the GIF data.
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+
+
+
+
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+
+// Set our network to devnet.
+const network = clusterApiUrl("devnet");
+
+// Controls how we want to acknowledge when a transaction is "done".
+const opts = {
+  preflightCommitment: "processed",
+};
 
 // Constants
 
 const App = () => {
-  const TEST_GIFS = [
-    "https://media.giphy.com/media/olAik8MhYOB9K/giphy.gif",
-    "https://media.giphy.com/media/Zgo2A2oOpbGhQdf09T/giphy.gif",
-    "https://media.giphy.com/media/HzX8pvJLdT01oyAIot/giphy.gif",
-    "https://media.giphy.com/media/Ry1MOAeAYXvRVQLPw3/giphy.gif",
-    "https://media.giphy.com/media/XCrNuIxkoVqQbG55MP/giphy.gif",
-    "https://media.giphy.com/media/w9t0aFMjahdxpKKvzN/giphy.gif",
-
-  ];
+  // const TEST_GIFS = [
+  //   "https://media.giphy.com/media/olAik8MhYOB9K/giphy.gif",
+  //   "https://media.giphy.com/media/Zgo2A2oOpbGhQdf09T/giphy.gif",
+  //   "https://media.giphy.com/media/HzX8pvJLdT01oyAIot/giphy.gif",
+  //   "https://media.giphy.com/media/Ry1MOAeAYXvRVQLPw3/giphy.gif",
+  //   "https://media.giphy.com/media/XCrNuIxkoVqQbG55MP/giphy.gif",
+  //   "https://media.giphy.com/media/w9t0aFMjahdxpKKvzN/giphy.gif",
+  // ];
   // State
   const [walletAddress, setWalletAddress] = useState(null);
   const [inputValue, setInputValue] = useState("");
@@ -58,18 +82,65 @@ const App = () => {
   };
 
   const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
-      setGifList([...gifList, inputValue]);
-      setInputValue('');
-    } else {
-      console.log('Empty input. Try again.');
+    if (inputValue.length === 0) {
+      console.log("No gif link given!")
+      return
+    }
+    setInputValue('');
+    console.log('Gif link:', inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+  
+      await program.rpc.addGif(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      console.log("GIF successfully sent to program", inputValue)
+  
+      await getGifList();
+    } catch (error) {
+      console.log("Error sending GIF:", error)
     }
   };
-
+  
   const onInputChange = (event) => {
     const { value } = event.target;
     setInputValue(value);
+  };
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
+
+  const createGifAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping");
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+      console.log(
+        "Created a new BaseAccount w/ address:",
+        baseAccount.publicKey.toString()
+      );
+      await getGifList();
+    } catch (error) {
+      console.log("Error creating BaseAccount account:", error);
+    }
   };
 
   const renderNotConnectedContainer = () => (
@@ -81,34 +152,50 @@ const App = () => {
     </button>
   );
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          sendGif();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Enter GIF link!"
-          value={inputValue}
-          onChange={onInputChange}
-        />
-        <button type="submit" className="cta-button submit-gif-button">
-          Submit
-        </button>
-      </form>
-      <div className="gif-grid">
-        {/* Map through gifList instead of TEST_GIFS */}
-        {gifList.map((gif) => (
-          <div className="gif-item" key={gif}>
-            <img src={gif} alt={gif} />
+  const renderConnectedContainer = () => {
+    // If we hit this, it means the program account hasn't been initialized.
+      if (gifList === null) {
+        return (
+          <div className="connected-container">
+            <button className="cta-button submit-gif-button" onClick={createGifAccount}>
+              Do One-Time Initialization For GIF Program Account
+            </button>
           </div>
-        ))}
-      </div>
-    </div>
-  );
+        )
+      } 
+      // Otherwise, we're good! Account exists. User can submit GIFs.
+      else {
+        return(
+          <div className="connected-container">
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                sendGif();
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Enter gif link!"
+                value={inputValue}
+                onChange={onInputChange}
+              />
+              <button type="submit" className="cta-button submit-gif-button">
+                Submit
+              </button>
+            </form>
+            <div className="gif-grid">
+              {/* We use index as the key instead, also, the src is now item.gifLink */}
+              {gifList.map((item, index) => (
+                <div className="gif-item" key={index}>
+                  <img src={item.gifLink} />
+                  <p style={{color: "white", fontSize: "10px"}}>Submitted by: {item.userAddress.toString()}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      }
+    }
 
   // UseEffects
   useEffect(() => {
@@ -123,24 +210,36 @@ const App = () => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
   }, []);
-  
+
+  const getGifList = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      console.log("Got the account", account);
+      setGifList(account.gifList);
+    } catch (error) {
+      console.log("Error in getGifList: ", error);
+      setGifList(null);
+    }
+  };
+
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching GIF list...');
-      
-      // Call Solana program here.
-  
-      // Set state
-      setGifList(TEST_GIFS);
+      console.log("Fetching GIF list...");
+      getGifList();
     }
   }, [walletAddress]);
 
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       {/* This was solely added for some styling fanciness */}
       <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
@@ -152,9 +251,8 @@ const App = () => {
           {/* We just need to add the inverse here! */}
           {walletAddress && renderConnectedContainer()}
         </div>
-        
 
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
